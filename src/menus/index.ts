b2b7cef5814aac044bce66844a21ac8bac0183f1
Daniel <90870585+VexyPT@users.noreply.github.com>
2025-01-23 03:02:42 +0000

    feat: ticket panel

--- src/menus/index.ts
+++ src/menus/index.ts
@@ -1,5 +1,9 @@
 import { configPermissionsMenu } from "./configPermissions/main.js";
+import { Panel } from "./ticketPanel/main.js";
 
 export const menus = {
-    configPermissions: configPermissionsMenu
+    configPermissions: configPermissionsMenu,
+    ticket: {
+        MainPanel: Panel
+    }
 }
---
+++ src/menus/ticketPanel/main.ts
@@ -0,0 +1,38 @@
+import { settings } from "#settings";
+import { brBuilder, createEmbed, createRow } from "@magicyan/discord";
+import { ButtonBuilder, ButtonStyle, Interaction, type InteractionReplyOptions } from "discord.js"
+
+export async function Panel<R>(interaction: Interaction): Promise<R> {
+    
+    const embedConfig = createEmbed({
+        color: settings.colors.azoxo,
+        author: {
+            name: interaction.guild!.name,
+            iconURL: interaction.guild!.iconURL() || undefined
+        },
+        description: brBuilder(
+            "Selecione uma opção de acordo com o assunto que deseja tratar com nossa equipe.",
+            "",
+            "**Lembre-se:** cada tipo de ticket é destinado exclusivamente ao tema escolhido, garantindo um atendimento mais eficiente."
+        )
+    });
+
+    const row = createRow(
+        new ButtonBuilder({
+            customId: "createTicket/support",
+            label: "Suporte",
+            style: ButtonStyle.Secondary,
+        }),
+        new ButtonBuilder({
+            customId: "createTicket/budget",
+            label: "Orçamento",
+            style: ButtonStyle.Primary,
+        }),
+    );
+
+    return ({
+        flags: ["Ephemeral"],
+        embeds: [embedConfig],
+        components: [row],
+    } satisfies InteractionReplyOptions) as R;
+}
