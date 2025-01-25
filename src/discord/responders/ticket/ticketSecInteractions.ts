17238cc354040a46645e7daba36feea00a8fc608
Daniel <90870585+VexyPT@users.noreply.github.com>
2025-01-25 19:49:54 +0000

    feat: notfUser functionality

--- src/discord/responders/ticket/ticketSecInteractions.ts
+++ src/discord/responders/ticket/ticketSecInteractions.ts
@@ -1,7 +1,9 @@
 import { createResponder, ResponderType } from "#base";
 import { guildDb, ticketDb } from "#database";
+import { res } from "#functions";
 import { menus } from "#menus";
-import { TextChannel } from "discord.js";
+import { createRow } from "@magicyan/discord";
+import { ButtonBuilder, ButtonStyle, TextChannel } from "discord.js";
 
 createResponder({
     customId: "ticketSec/:type/:action",
@@ -76,12 +78,72 @@ createResponder({
     
                             return;
                         }
-                        case "NotfUser":
-                            // Notificar usuário
-                            break;
-                        case "AssumTicket":
-                            // Assumir ticket
-                            break;
+                        case "NotfUser": {
+                            const ticketChannel = interaction.channel as TextChannel;
+                            if(!ticketChannel) {
+                                interaction.reply({
+                                    content: "Este canal não é um canal de ticket válido",
+                                    flags: ["Ephemeral"],
+                                });
+                                return;
+                            }
+                            const ticketChannelButton = createRow(
+                                new ButtonBuilder({
+                                    style: ButtonStyle.Link,
+                                    label: "Ir para o ticket",
+                                    url: ticketChannel.url,
+                                })
+                            );
+
+                            const ticketData = ticketDb.get(`tickets.${ticketChannel.id}`);
+                            if(!ticketData) {
+                                interaction.reply({
+                                    content: "Não foi possível encontrar os dados do ticket.",
+                                    flags: ["Ephemeral"],
+                                });
+                                return;
+                            }
+
+                            const creatorId = ticketData.createdBy;
+                            if(!creatorId) {
+                                interaction.reply({
+                                    content: "Não foi possível indentificar o criador do ticket.",
+                                    flags: ["Ephemeral"],
+                                });
+                                return;
+                            }
+
+                            const creator = await guild!.members.fetch(creatorId);
+
+                            if(creator) {
+                                // Tentar enviar a mensagem
+                                creator.send(
+                                    res.warning(`Seu ticket foi atendido! Nossa equipe de suporte está te aguardando em ${ticketChannel}`, {
+                                        components: [ticketChannelButton],
+                                        ephemeral: true,
+                                    })
+                                ).then(() => {
+                                    interaction.reply({
+                                        content: "O usuário foi notificado com sucesso!",
+                                        flags: ["Ephemeral"],
+                                    });
+                                }).catch(() => {
+                                    interaction.reply({
+                                        content: "Não foi possível notificar o usuário, as DM's do usuário estão desativadas.",
+                                        flags: ["Ephemeral"],
+                                    });
+                                })
+                            } else {
+                                interaction.reply({
+                                    content: "Não foi possível notificar o usuário",
+                                    flags: ["Ephemeral"],
+                                });
+                            }
+                            return;
+                        }
+                        case "AssumTicket": {
+                            
+                        }
                         case "createCall":
                             // Criar chamada
                             break;
@@ -196,7 +258,7 @@ createResponder({
                                     });
                                     return;
                                 }
-                                
+
                                 if(!ticketData.users || !ticketData.users.includes(userIdToRemove)) {
                                     interaction.reply({
                                         content: "Este usuário não está no ticket.",
