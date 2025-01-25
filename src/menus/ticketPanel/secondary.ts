3d429ed49d8ad918a7189ad7c2b42e3a5b289163
Daniel <90870585+VexyPT@users.noreply.github.com>
2025-01-25 17:53:37 +0000

    Create secondary.ts

---
+++ src/menus/ticketPanel/secondary.ts
@@ -0,0 +1,72 @@
+import { ButtonBuilder, ButtonStyle, Interaction, type InteractionReplyOptions } from "discord.js";
+import { createEmbed, createRow, brBuilder } from "@magicyan/discord";
+import { settings } from "#settings";
+
+export function secondaryMenu<R>(interaction: Interaction): R {
+    
+    const embedPanel = createEmbed({
+        color: settings.colors.azoxo,
+        description: brBuilder(
+            `# Atendimento ${interaction.guild!.name}`,
+            "- Caso deseje cancelar ou sair, basta __clicar no botão vermelho__.",
+            "- Lembrando que os __botões cinzas__ são exclusivos para a equipe de suporte!"
+        )
+    });
+
+    const row1 = createRow(
+        new ButtonBuilder({
+            customId: "ticketSec/button/addUser",
+            label: "Adicionar Usuário",
+            style: ButtonStyle.Secondary,
+        }),
+        new ButtonBuilder({
+            customId: "ticketSec/button/removeUser",
+            label: "Remover Usuário",
+            style: ButtonStyle.Secondary,
+        }),
+        new ButtonBuilder({
+            customId: "ticketSec/button/NotfUser",
+            label: "Notíficar Usuário",
+            style: ButtonStyle.Secondary,
+        }),
+        new ButtonBuilder({
+            customId: "ticketSec/button/AssumTicket",
+            label: "Assumir Ticket",
+            style: ButtonStyle.Secondary,
+        }),
+        new ButtonBuilder({
+            customId: "ticketSec/button/createCall",
+            label: "Criar chamada",
+            style: ButtonStyle.Secondary,
+        }),
+    );
+
+    const row2 = createRow(
+        new ButtonBuilder({
+            customId: "ticketSec/button/moveChannel",
+            label: "Mover Canal",
+            style: ButtonStyle.Secondary,
+        }),
+        new ButtonBuilder({
+            customId: "ticketSec/button/defPriority",
+            label: "Definir Prioridade",
+            style: ButtonStyle.Secondary,
+        }),
+        new ButtonBuilder({
+            customId: "ticketSec/button/leaveTicket",
+            label: "Sair ou Cancelar",
+            style: ButtonStyle.Danger,
+        }),
+        new ButtonBuilder({
+            customId: "ticketSec/button/finalizeTicket",
+            label: "Finalizar",
+            style: ButtonStyle.Primary,
+        })
+    );
+
+    return ({
+        embeds: [embedPanel],
+        components: [row1, row2],
+        flags: []
+    } satisfies InteractionReplyOptions) as R;
+}
